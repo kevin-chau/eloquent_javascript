51b092e2fe36fc6dc0eefb41ecea5f25feca518a
kevin-chau <kevin@kevinchau.com>
2026-01-08 06:28:25 -0800

    Working example addition program.

---
+++ introduction/add.js
@@ -0,0 +1,9 @@
+// Simple example program for adding the first 10 numbers
+// Execute with `node add.js`
+let total = 0, count = 1;
+while (count <= 10) {
+    total += count;
+    count += 1;
+}
+console.log(total)
+// -> 55
